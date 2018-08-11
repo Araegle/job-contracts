20ad550673e3141ad36163861a2011ffd2c8bd49
Martín Triay <martriay@gmail.com>
2018-08-11 19:29:06 +0100

    Fix contract errors

--- job/contracts/JobApp.sol
+++ job/contracts/JobApp.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 
 import "@aragon/os/contracts/apps/AragonApp.sol";
 import "./ENS.sol";
@@ -6,10 +6,10 @@ import "./ENS.sol";
 contract JobApp is AragonApp {
 
     /// Events
-    event Applied(bytes32 node, string jobName);
-    event Hired(bytes32 node, string jobName);
-    event JobOpened(string indexed name, string description);
-    event JobClosed(string indexed name, bytes32 _hiree);
+    event Applied(bytes32 node, bytes32 jobName);
+    event Hired(bytes32 node, bytes32 jobName);
+    event JobOpened(bytes32 indexed name, string description);
+    event JobClosed(bytes32 indexed name, bytes32 _hiree);
 
     /// State
     ENS ens = ENS(0x314159265dD8dbb310642f98f50C066173C1259b);
@@ -17,6 +17,7 @@ contract JobApp is AragonApp {
     struct Job {
         string description;
         bool open;
+        bool exists;
     }
 
     mapping (bytes32 => Job) internal jobs;
@@ -27,11 +28,7 @@ contract JobApp is AragonApp {
     bytes32 constant public OWNER_ROLE = keccak256("OWNER_ROLE");
     bytes32 constant public MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
-    /**
-     * @notice Apply for a job by `jobName`
-     * @param step Amount to increment by
-     */
-    function apply(bytes32 node, string jobName) external {
+    function apply(bytes32 node, bytes32 jobName) external {
         require(msg.sender == nodeOwner(node));
         require(!hasApplied(node, jobName));
         require(jobs[keccak256(jobName)].open);
@@ -39,34 +36,26 @@ contract JobApp is AragonApp {
         Applied(node, jobName);
     }
 
-    /**
-     * @notice Open a job the counter by `step`
-     * @param step Amount to decrement by
-     */
-    function openJob(string _name, string _description) external auth(OWNER_ROLE) {
-        require(jobs[_name] == 0);
+    function openJob(bytes32 _name, string _description) external auth(OWNER_ROLE) {
+        require(! jobs[_name].exists);
+
         Job memory job = Job({
             description: _description,
-            open: true
+            open: true,
+            exists: true
         });
+
         jobs[keccak256(_name)] = job;
         JobOpened(_name, _description);
     }
 
-    /**
-     * @notice Open a job the counter by `step`
-     * @param step Amount to decrement by
-     */
-    function closeJob(string _name, string _node) external auth(OWNER_ROLE) {
+    function closeJob(bytes32 _name, bytes32 _node) external auth(OWNER_ROLE) {
+        require(jobs[keccak256((_name))].open);
         jobs[keccak256(_name)].open = false;
         JobClosed(_name, _node);
     }
 
-    /**
-     * @notice Apply for a job by `jobName`
-     * @param step Amount to increment by
-     */
-    function hire(bytes32 node, string jobName) external auth(MANAGER_ROLE) {
+    function hire(bytes32 node, bytes32 jobName) external auth(MANAGER_ROLE) {
         require(hasApplied(node, jobName));
         require(!hasBeenHired(node, jobName));
         require(jobs[keccak256(jobName)].open);
@@ -74,18 +63,18 @@ contract JobApp is AragonApp {
         Hired(node, jobName);
     }
 
-    function getJobDetails(string _name) public view returns(string, string) {
-        require(jobs[_name] != 0);
-        string description = jobs[keccak256(_name)].description;
-        string status = jobs[keccak256(_name)].open ? "Open" : "Closed";
+    function getJobDetails(bytes32 _name) public view returns(string, string) {
+        require(jobs[_name].exists);
+        string memory description = jobs[keccak256(_name)].description;
+        string memory status = jobs[keccak256(_name)].open ? "Open" : "Closed";
         return (description, status);
     }
 
-    function hasApplied(bytes32 _node, string _job) public view returns(bool) {
+    function hasApplied(bytes32 _node, bytes32 _job) public view returns(bool) {
         return applications[keccak256(_node, _job)];
     }
 
-    function hasBeenHired(bytes32 _node, string _job) public view returns(bool) {
+    function hasBeenHired(bytes32 _node, bytes32 _job) public view returns(bool) {
         return hired[keccak256(_node, _job)];
     }
 
